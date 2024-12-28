a4e95233da58299209db3bc7918faaf2133f3850
scholarnzl <sam@Sam-Laptop>
2024-12-28 08:48:26 -0800

    initial commit

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
-        pygame.Surface.fill(screen,(0,0,0))
+        screen.fill("black")
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
           if event.type == pygame.QUIT:
             return
